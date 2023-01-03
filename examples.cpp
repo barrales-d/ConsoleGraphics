6fb7603d2405b4ffa0a84eb1445e23e0a460e3c8
barrales-d <diego.barrales@csu.fullerton.edu>
2023-01-02 19:00:37 -0800

    Created example and removed TODO

--- examples.cpp
+++ examples.cpp
@@ -55,13 +55,24 @@ void circles() {
   image.show();
 }
 
+void color_palette() {
+  const std::string text = "C O N S O L E @ G R A P H I C S ";
+  CG_Image image(CG_FORE_COLORS_SIZE * 2, CG_BACK_COLORS_SIZE);
+
+  for (int x = 0; x < CG_FORE_COLORS_SIZE * 2; x++) {
+    image.fill_rect(x, 0, 2, CG_BACK_COLORS_SIZE, CG_BACK_COLORS[x / 2]);
+  }
+  for (int y = 0; y < CG_BACK_COLORS_SIZE; y++) {
+    image.draw_text(0, y, text, CG_FORE_COLORS[y % CG_FORE_COLORS_SIZE]);
+  }
+  image.show();
+}
 /* TODOs
   //  Explore Testcases and unit tests by creating a class TestCase friend of CG_Image
   //  Update show() to act more like a batch renderer? (instead of pixel by pixel it prints row by row)
   //  Explore triangle scaler? 
   //  Animations?
   //  Create cooler examples like circles() which is a bulls-eye 
-  //  implement draw_text() method for CG_Image
 
 */
 
