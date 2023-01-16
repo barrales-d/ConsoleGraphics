8ba73999d8b7f3ab2c8815af8c56b3b11ef74d7e
barrales-d <diego.barrales@csu.fullerton.edu>
2023-01-15 22:13:06 -0800

    fixed all example with new scalings

--- examples.cpp
+++ examples.cpp
@@ -1,9 +1,9 @@
-
-#include "./CG/ColorCodes.hpp"
 #include "./CG/CG_Image.hpp"
+#include "./CG/ColorCodes.hpp"
+#include <iostream>
 
-#define WIDTH 150
-#define HEIGHT 28
+#define WIDTH 45
+#define HEIGHT 20
 
 void checker() {
   CG_Image image(WIDTH, HEIGHT);
@@ -21,7 +21,6 @@ void checker() {
   }
   image.show();
 }
-
 void triangle() {
   CG_Image image(WIDTH, HEIGHT);
   int x1 = 0, y1 = HEIGHT;
@@ -35,37 +34,57 @@ void all() {
   CG_Image image(WIDTH, HEIGHT);
   int x1 = 0, y1 = HEIGHT;
   int x2 = WIDTH / 2, y2 = 0;
-  int x3 = WIDTH, y3 = HEIGHT;
+  int x3 = WIDTH;
   int x4 = WIDTH / 2, y4 = HEIGHT / 2;
   image.fill_background(ColorCodes::bg_Black);
-  image.fill_rect(x2 + 20, y2, 15, 15, ColorCodes::bg_Blue);
+  image.fill_rect(x2 + 10, y2, 10, 10, ColorCodes::bg_Blue);
   image.fill_circle(x4, y4, 10, ColorCodes::bg_LightRed);
   image.draw_line(x1, y2, x3, y1, ColorCodes::bg_Magenta);
   image.fill_triangle(x1, y4, x2, y2, x4, y4, ColorCodes::bg_Green);
   image.show();
 }
-
 void circles() {
   CG_Image image(WIDTH, HEIGHT);
   int cx = WIDTH / 2;
   int cy = HEIGHT / 2;
+
   for (int r = 20; r >= 0; r -= 2) {
     image.fill_circle(cx, cy, r, CG_BACK_COLORS[r % CG_BACK_COLORS_SIZE]);
   }
+  image.show();
+}
 
+void insert_text(const std::string &text) {
+  CG_Image image(WIDTH, HEIGHT);
+  int x = WIDTH / 2 - text.size() / 2;
+  int y = HEIGHT / 2;
+
+  // //  change the background of text
+  // image.fill_rect(x, y, text.size(), 1, ColorCodes::bg_Magenta);
+  //  set text color
+  image.draw_text(x, y, text, ColorCodes::fg_Blue);
+  image.draw_text(x, y + 1, text, ColorCodes::fg_Blue, ColorCodes::bg_Black);
   image.show();
 }
 
 void color_palette() {
   const std::string text = "C O N S O L E @ G R A P H I C S ";
-  CG_Image image(CG_FORE_COLORS_SIZE * 2, CG_BACK_COLORS_SIZE);
+  CG_Image image(CG_FORE_COLORS_SIZE, CG_BACK_COLORS_SIZE);
 
-  for (int x = 0; x < CG_FORE_COLORS_SIZE * 2; x++) {
-    image.fill_rect(x, 0, 2, CG_BACK_COLORS_SIZE, CG_BACK_COLORS[x / 2]);
+  for (int x = 0; x < CG_FORE_COLORS_SIZE; x++) {
+    image.fill_rect(x, 0, 1, CG_BACK_COLORS_SIZE, CG_BACK_COLORS[x]);
   }
   for (int y = 0; y < CG_BACK_COLORS_SIZE; y++) {
     image.draw_text(0, y, text, CG_FORE_COLORS[y % CG_FORE_COLORS_SIZE]);
   }
+  image.show();
+}
+void point() {
+  CG_Image image(WIDTH, HEIGHT);
+  image.fill_point(0, 0, ColorCodes::bg_Red);
+  image.fill_point(0, 3, ColorCodes::bg_Blue);
+  image.fill_point(3, 0, ColorCodes::bg_Green);
+  image.fill_point(WIDTH - 1, HEIGHT - 1, ColorCodes::bg_Black);
   image.show();
 }
 /* TODOs
@@ -78,6 +97,11 @@ void color_palette() {
 */
 
 int main() {
+  checker();
   circles();
+  triangle();
+  all();
+  color_palette();
+  point();
   return 0;
 }
