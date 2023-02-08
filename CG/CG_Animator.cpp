2dfafd2593e6ce9971889c3167b67a215bed4c16
barrales-d <dbarrales06@gmail.com>
2023-02-08 12:52:51 -0800

    Add TODOs

--- CG/CG_Animator.cpp
+++ CG/CG_Animator.cpp
@@ -4,7 +4,10 @@
 
 void CG_Animator::play() {
     while(1) {
+        //TODO: figure out delta time
+        //TODO: move terminal cursor to (0, 0) I have it in the CG_Image cpp file
         m_animation->onUpdate(1/20);
         m_animation->onRender();
+
     }
 }
