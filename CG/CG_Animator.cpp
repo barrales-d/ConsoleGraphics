54ded2ca1633ff57c795d0cad5cc6362fa60c384
barrales-d <dbarrales06@gmail.com>
2023-02-09 09:34:39 -0800

    Removed TODO for delta time

--- CG/CG_Animator.cpp
+++ CG/CG_Animator.cpp
@@ -3,17 +3,14 @@
 void CG_Animator::play() {
     auto current = std::chrono::steady_clock::now();
     while(1) {
-        //TODO: figure out delta time
         //TODO: move terminal cursor to (0, 0) I have it in the CG_Image cpp file
-
         std::chrono::duration<float> deltaTime = std::chrono::steady_clock::now() - current;
         float dt = deltaTime.count();
 
         m_animation->onUpdate(dt);
         m_animation->onRender();
         
-        usleep(1000 * 1000 / FPS);
         current = std::chrono::steady_clock::now();
-
+        usleep(1000 * 1000 / FPS);
     }
 }
