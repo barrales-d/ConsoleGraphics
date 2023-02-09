ef033eb07a4ef31f9427c92cad1a232352f7fe33
barrales-d <dbarrales06@gmail.com>
2023-02-09 11:09:54 -0800

    Create another todo for animation play function

--- CG/CG_Animator.cpp
+++ CG/CG_Animator.cpp
@@ -5,6 +5,7 @@ void CG_Animator::play() {
     //  move cursor to bottom right corner of image to reset properly AND use \e[?25l to hide cursor
     std::printf("\x1b[%dC\x1b[%dB\e[?25l", m_animation->getWidth(), m_animation->getHeight());
 
+    //TODO: create a way to exit the loop and set the cursor to the bottom right again 
     while(1) {
         //  move cursor to top left corner of image every frame
         std::printf("\x1b[%dD\x1b[%dA", m_animation->getWidth(), m_animation->getHeight());
