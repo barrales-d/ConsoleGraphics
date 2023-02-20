f3a42802d7d61f249616252320a18b478e11262f
barrales-d <dbarrales06@gmail.com>
2023-02-20 15:54:41 -0800

    fixed examples

--- CG/Image.hpp
+++ CG/Image.hpp
@@ -7,6 +7,7 @@
 #define WIDTH_SCALER 2 
 
 namespace CG {
+
   class Pixel {
   public: 
     Color fg_color;
@@ -20,33 +21,33 @@ namespace CG {
   };//  Pixel
 
 
-class Image {
-protected:
-  int m_width;
-  int m_height;
-  Pixel *m_pixels;
+  class Image {
+  protected:
+    int m_width;
+    int m_height;
+    Pixel *m_pixels;
 
-  void swap(int &a, int &b);
-  void sort_by_x(int &x1, int &y1, int &x2, int &y2, int &x3, int &y3);
-public:
-  Image(const int width, const int height);
-  ~Image(); 
+    void swap(int &a, int &b);
+    void sort_by_x(int &x1, int &y1, int &x2, int &y2, int &x3, int &y3);
+  public:
+    Image(const int width, const int height);
+    ~Image(); 
 
-  void fill_background(const Color &bg_color = Color::darkgrey);
+    void fill_background(const Color &bg_color = Color::darkgrey);
 
-  void show();
+    void show();
 
-  void fill_point(int x, int y, const Color &bg_color);
-  void fill_rect(int x, int y, int wd, int ht, const Color &bg_color);
-  void fill_circle(int x, int y, int r, const Color &bg_color);
-  void fill_triangle(int x1, int y1, int x2, int y2, int x3, int y3, const Color &bg_color);
+    void fill_point(int x, int y, const Color &bg_color);
+    void fill_rect(int x, int y, int wd, int ht, const Color &bg_color);
+    void fill_circle(int x, int y, int r, const Color &bg_color);
+    void fill_triangle(int x1, int y1, int x2, int y2, int x3, int y3, const Color &bg_color);
 
-  void draw_line(int x1, int y1, int x2, int y2, const Color &bg_color);
-  void draw_text(int x, int y, const std::string &text, const Color &fg_color);
-  void draw_text(int x, int y, const std::string &text, const Color &fg_color, const Color &bg_color);
-// 
-  static Image combine_image(const Image& img1, const Image& img2);
-  friend class TestCase;
-};//  Image
+    void draw_line(int x1, int y1, int x2, int y2, const Color &bg_color);
+    void draw_text(int x, int y, const std::string &text, const Color &fg_color);
+    void draw_text(int x, int y, const std::string &text, const Color &fg_color, const Color &bg_color);
+  // 
+    static Image combine_image(const Image& img1, const Image& img2);
+    friend class TestCase;
+  };//  Image
 }
 #endif //  __Image_HPP__
