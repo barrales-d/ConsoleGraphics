71e207723577c1d5d15b5d4567160c43a6c8df5d
barrales-d <dbarrales06@gmail.com>
2023-04-13 09:53:04 -0700

    Clearing the branch

--- CG/Animations/animations.cpp
+++
@@ -1,9 +0,0 @@
-#include "Rainbow.hpp"
-#include "Bouncingball.hpp"
-
-#define WIDTH 30
-#define HEIGHT 20
-int main() {
-    BouncingBall animation(WIDTH, HEIGHT);
-    animation.play_animation();
-}
