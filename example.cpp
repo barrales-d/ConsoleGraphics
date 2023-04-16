9c05f8b8b2087de4be8748d3073947febe1c6760
barrales-d <dbarrales06@gmail.com>
2023-04-15 19:02:07 -0700

    using Bezier Curves!!!

--- example.cpp
+++ example.cpp
@@ -15,32 +15,35 @@ int main()
 	image.fill_rect(1, 1, 11, 20, Color(255, 20, 20, 128));
 	image.fill_circle(9, 9, 4, Colors::magenta);
 	image.fill_ellipse(9, 9, 8, 4, Color(0, 50, 255, 128));
-	image.fill_line_rect(3, 6, 5, 4, Colors::lightgreen);
+	image.fill_rect_line(3, 6, 5, 4, Colors::lightgreen);
 	image.fill_triangle(20, 0, 10, 10, 20, 20, Colors::red, Colors::green, Colors::blue);
 	image.show();
 	
 	ansi_codes::move_cursor(20, 20 * 2);
 	
-	Image image2 = image;
+	Image image2(20, 20);
 	image2.fill_background();
-	image.fill_triangle(20, 0, 10, 10, 20, 20, Colors::magenta, Colors::yellow, Colors::cyan);
-	image.fill_triangle(0, 20, 10, 10, 20, 20, Colors::magenta, Colors::yellow, Colors::cyan);
-	image.fill_triangle(20, 0, 10, 10, 0, 0, Colors::magenta, Colors::yellow, Colors::cyan);
-	image.fill_triangle(0, 20, 10, 10, 0, 0, Colors::magenta, Colors::yellow, Colors::cyan);
+	image2.fill_triangle(20, 0, 10, 10, 20, 20, Colors::magenta, Colors::yellow, Colors::cyan);
+	image2.fill_triangle(0, 20, 10, 10, 20, 20, Colors::magenta, Colors::yellow, Colors::cyan);
+	image2.fill_triangle(20, 0, 10, 10, 0, 0, Colors::magenta, Colors::yellow, Colors::cyan);
+	image2.fill_triangle(0, 20, 10, 10, 0, 0, Colors::magenta, Colors::yellow, Colors::cyan);
 	image2.show();
 
 	image2.fill_background();
-	image.fill_triangle(20, 0, 10, 10, 20, 20, Colors::yellow, Colors::magenta, Colors::cyan);
-	image.fill_triangle(0, 20, 10, 10, 20, 20, Colors::yellow, Colors::magenta, Colors::cyan);
-	image.fill_triangle(20, 0, 10, 10, 0, 0, Colors::yellow, Colors::magenta, Colors::cyan);
-	image.fill_triangle(0, 20, 10, 10, 0, 0, Colors::yellow, Colors::magenta, Colors::cyan);
+	image2.fill_triangle(20, 0, 10, 10, 20, 20, Colors::yellow, Colors::magenta, Colors::cyan);
+	image2.fill_triangle(0, 20, 10, 10, 20, 20, Colors::yellow, Colors::magenta, Colors::cyan);
+	image2.fill_triangle(20, 0, 10, 10, 0, 0, Colors::yellow, Colors::magenta, Colors::cyan);
+	image2.fill_triangle(0, 20, 10, 10, 0, 0, Colors::yellow, Colors::magenta, Colors::cyan);
 	image2.show();
 
 	image2.fill_background();
-	image.fill_triangle(20, 0, 10, 10, 20, 20, Colors::cyan, Colors::yellow, Colors::magenta);
-	image.fill_triangle(0, 20, 10, 10, 20, 20, Colors::cyan, Colors::yellow, Colors::magenta);
-	image.fill_triangle(20, 0, 10, 10, 0, 0, Colors::cyan, Colors::yellow, Colors::magenta);
-	image.fill_triangle(0, 20, 10, 10, 0, 0, Colors::cyan, Colors::yellow, Colors::magenta);
+	image2.fill_triangle(20, 0, 10, 10, 20, 20, Colors::cyan, Colors::yellow, Colors::magenta);
+	image2.fill_triangle(0, 20, 10, 10, 20, 20, Colors::cyan, Colors::yellow, Colors::magenta);
+	image2.fill_triangle(20, 0, 10, 10, 0, 0, Colors::cyan, Colors::yellow, Colors::magenta);
+	image2.fill_triangle(0, 20, 10, 10, 0, 0, Colors::cyan, Colors::yellow, Colors::magenta);
+
+	image2.fill_bezier_curve_line(Vec2(0, 0), Vec2(0, 20), Vec2(20, 20), Colors::black);
+	image2.fill_bezier_curve_line(Vec2(0, 0), Vec2(20, 0), Vec2(20, 20), Colors::black);
 	image2.show();
 
 	return 0;
